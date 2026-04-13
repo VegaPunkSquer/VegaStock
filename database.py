c8f30c8633da7a5f8aeb4fee65f6f714b94f27da
Vega <serginhu.silva@gmail.com>
2026-04-13 18:59:45 -0300

    feat: Enable pool pre-ping for PostgreSQL engine to improve connection reliability

--- database.py
+++ database.py
@@ -10,7 +10,7 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Motor do PostgreSQL (Não precisa mais do connect_args)
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
